e54212d31cfe6a354d7329f68a0ba900053af063
App Academy Student <student@appacademy.io>
2019-06-24 18:05:00 -0700

    final commit for the day

---
+++ TODOList/frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return(
+    <h1>ToDo List *components</h1>
+  );
+};
+ export default App;
+
--- TODOList/frontend/todo_redux.jsx
+++ TODOList/frontend/todo_redux.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store.js';
 import * as actions from './actions/todo_actions.js';
 
+import App from './components/app.jsx';
+
 document.addEventListener('DOMContentLoaded',() => {
   const root = document.getElementById("content");
   ReactDOM.render(
-     <h1>TODO App</h1>,
+     <App />,
      root 
      );
      
